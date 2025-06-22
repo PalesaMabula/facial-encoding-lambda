f02f1ce27bfe7cc46748ade412ac6dc8414f2ed5
Palesa Mabula <111003405+PalesaMabula@users.noreply.github.com>
2025-06-22 18:56:46 +0200

    Update test_lambda.py

--- test_lambda.py
+++ test_lambda.py
@@ -2,7 +2,8 @@
 import json
 from lambda_function import handler
 
-# Replace this with a real base64 image string for actual test
+# Replace this image
+
 fake_event = {
     "body": json.dumps({
         "image_url": "https://raw.githubusercontent.com/ageitgey/face_recognition/master/examples/obama.jpg"
